fccc76d5d7f7c15e80645eb3246030302f983437
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-05-18 18:55:10 +0700

    Configuration

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('BASE_URL', 'localhost:8888/percetakan/');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'percetakan');
