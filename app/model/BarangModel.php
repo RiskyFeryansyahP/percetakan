1510058a630492dfeb56d6b826a4e88aa16f587b
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-05-19 22:39:35 +0700

    added method Model to add barang and delete barang

--- app/model/BarangModel.php
+++ app/model/BarangModel.php
@@ -14,4 +14,40 @@ class BarangModel
         $this->db->query("SELECT * FROM barang");
         return $this->db->resultSet();
     }
+
+    public function addBarang($kode_barang, $nama_barang, $stock)
+    {
+        $this->db->query("INSERT INTO barang VALUES(:kode_barang, :nama_barang, :stock)");
+
+        $this->db->bind('kode_barang', $kode_barang);
+        $this->db->bind('nama_barang', $nama_barang);
+        $this->db->bind('stock', $stock);
+
+        $result = $this->db->execute();
+        if($result)
+        {   
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public function deleteBarang($kode_barang)
+    {
+        $this->db->query("DELETE FROM barang WHERE kode_barang=:kode_barang");
+
+        $this->db->bind('kode_barang', $kode_barang);
+
+        $result = $this->db->execute();
+        if($result)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
