6593166a0db22a7ea7050b5cfcce4e4461ceaa8a
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-05-19 22:38:31 +0700

    Added method AddOneBarang and DeleteOneBarang

--- app/controller/Barang.php
+++ app/controller/Barang.php
@@ -2,8 +2,32 @@
 
 class Barang extends Controller
 {
-    public function getAllBarang()
+    public function AddOneBarang()
     {
-        
+        $kode_barang    = $_POST['kode_barang'];
+        $nama_barang    = $_POST['nama_barang'];
+        $stock          = $_POST['stock'];
+        $result = $this->model('BarangModel')->addBarang($kode_barang, $nama_barang, $stock);
+        if($result)
+        {
+            header("Location: http://localhost:8888/Percetakan/dashboard/barang");
+        }
+        else
+        {
+            echo "Gagal";
+        }
+    }
+
+    public function DeleteOneBarang($kode_barang)
+    {
+        $result = $this->model('BarangModel')->deleteBarang($kode_barang);
+        if($result)
+        {
+            header("Location: http://localhost:8888/Percetakan/dashboard/barang");
+        }
+        else
+        {
+            echo "Gagal!";
+        }
     }
 }
