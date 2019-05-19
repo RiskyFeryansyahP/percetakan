2c7639f6005550079ed974a788ee9db65e0ababd
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-05-19 23:54:19 +0700

    modal show and modal edit javascript

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,18 @@
 document.querySelectorAll('.modal-button').forEach((element) => {
     element.addEventListener('click', () => {
         const target = document.querySelector(element.getAttribute('data-target'))
+        const id = element.getAttribute('data-modal') // mengambil value dari data-modal(seperti data-target) di modal-button
+        if(id != null)
+        {
+            const data = id.split("-") // pisah tiap (-) kedalam array
+            console.log(data)
+            console.log(id)
+
+            document.getElementById("kode_barang").value = data[0]
+            document.getElementById("nama_barang").value = data[1]
+            document.getElementById("stock").value = data[2]
+        }
+
         target.classList.add('is-active')
 
         target.querySelector('.modal-close').addEventListener('click', () => {
